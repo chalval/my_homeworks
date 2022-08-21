c1abaaecce9f46879367dba1ce46083ee4099fa2
chalval <chalval@mail.ru>
2022-08-21 16:39:49 +0300

    4 zadachi pervogo seminara

--- homeworks/homework1/Program.cs
+++ homeworks/homework1/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2) 
+{
+    Console.WriteLine($"First number {num1} > second number {num2}");
+}
+else if (num1<num2) 
+{
+    Console.WriteLine($"First number {num1} < second number {num2}");
+}
+else 
+{
+    Console.WriteLine($"First number {num1} = second number {num2}");
+}
+
+*/
+
+/*
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2>max)
+{
+    max = num2;
+}
+if (num3>max)
+{
+    max = num3;
+}
+Console.WriteLine($"Largest number {max}");
+
+*/
+
+/*
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+    Console.WriteLine($"number {num} is even");
+}
+else
+{
+    Console.WriteLine($"number {num} is not even");
+}
+
+*/
+
+/*
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Even numbers from 1 to N: ");
+int index=1;
+while (index <= n)
+{
+    if (index%2 == 0)
+    {
+        Console.Write(index + " ");
+    }
+    index++;
+}
+Console.WriteLine();
+
+*/
+
