6dc3e289b1d4810395bf5580bfdad88b221481f4
chalval <chalval@mail.ru>
2022-08-28 02:05:00 +0300

    2 zadacha

--- homeworks/homework3/Program.cs
+++ homeworks/homework3/Program.cs
@@ -35,9 +35,38 @@ else Console.WriteLine($"Number {number} is not a palindrome");
 
 */
 
-Задача 21
+//Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+
+double distance (double x1,double y1,double z1,double x2,double y2,double z2)
+{
+    double dis=Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+    return dis;
+}
+
+Console.Write("Input coordinates X of the first point: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinates Y of the first point: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinates Z of the first point: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinates X of the second point: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinates Y of the second point: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinates Z of the second point: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Distance between two points: "+ distance(x1,y1,z1,x2,y2,z2));
+
+*/
+
+
+
+
+
+
+
 
 
 
