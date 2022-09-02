fc1c20648e0d3df0c4a265ce46b8eb96ffcb7a72
chalval <chalval@mail.ru>
2022-09-02 17:54:59 +0300

    1s

--- homeworks/homework5/Program.cs
+++ homeworks/homework5/Program.cs
@@ -1,5 +1,33 @@
 ﻿
+//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
 
+/*
+
+void even(int[]a)
+{
+    for (int i=0; i<a.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {a[i]}");
+    }
+
+    int n=0;
+    for (int i=0; i<a.Length; i++)
+    {
+        if (a[i]%2==0) n++;
+    }
+    Console.WriteLine($"The number of even numbers in the array = {n}");
+}
+
+
+Console.Write("Enter the number of array elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int[size];
+for (int i=0; i<size; i++)
+    arr[i]=new Random().Next(100, 1000);
+even(arr);
+
+*/
 
 
 
