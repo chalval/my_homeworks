4056d71758f62b1db1351c531b9a6b34f5922508
chalval <chalval@mail.ru>
2022-09-14 17:05:10 +0300

    3

--- homeworks/homework8/Program.cs
+++ homeworks/homework8/Program.cs
@@ -130,6 +130,65 @@ void PrintArray(int[,]array)
     
 */
 
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+/*
+
+int [,] CreateRandomArray (int m, int n )
+{
+    Console.WriteLine();
+    int [,] array = new int [m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0,10);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j].ToString("0,0"+ "  "));
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+ 
+
+
+int[,] arr1 = CreateRandomArray(2, 2);
+int[,] arr2 = CreateRandomArray(2, 2);
+Console.WriteLine("Matrix multiplication 2X2");
+Console.WriteLine();
+Console.WriteLine("A");
+PrintArray(arr1);
+Console.WriteLine("B");
+PrintArray(arr2);
+Console.WriteLine("C");
+int [,] arrayC = new int [2, 2];
+arrayC[0, 0] = arr1[0, 0] * arr2[0, 0] +  arr1[0, 1] * arr2[1, 0];
+arrayC[0, 1] = arr1[0, 0] * arr2[0, 1] +  arr1[0, 1] * arr2[1, 1];
+arrayC[1, 0] = arr1[1, 0] * arr2[0, 0] +  arr1[1, 1] * arr2[1, 0];
+arrayC[1, 1] = arr1[1, 0] * arr2[0, 1] +  arr1[1, 1] * arr2[1, 1];
+PrintArray(arrayC);
+
+*/
+
+
+
+
+
+
+
+
 
 
 
